9016c0f35c4ee2fac36a80cd677bd3a8fa00c8fd
Nantawat Rattanaruangviman <69116458+NantawatTOMOHA@users.noreply.github.com>
2024-03-12 21:24:30 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# analyze_email --add <address>@gmail.com
 import sys
 
 def add_data(file_path, new_data):
@@ -36,34 +35,60 @@ def list_data(file_path):
             print(line.strip())
 
 def main():
-    if len(sys.argv) < 2 or not sys.argv[1].startswith("--"):
-        print("Usage:--add|--remove|--list [new_data]")
+    if len(sys.argv) < 2 or (not sys.argv[1].startswith("--") and not sys.argv[1].startswith("-")) or len(sys.argv) > 3:
+        print("Error: Invalid option or argument. Use --help for usage information.")
         sys.exit(1)
 
     command = sys.argv[1].lower()
-    file_path = "/home/Email_management-Python/cache/list_email.txt"
+    file_path = "/home/Email_management-Python/src/list_email.txt"
+    
 
-    if command == "--add":
-        if len(sys.argv) < 2:
-            print("Usage:--add [new_data]")
+    if command == "--add" or command == "-a":
+        if len(sys.argv[2]) > 76:
+            print("Error: The email address is too long (maximum 76 characters).")
+            sys.exit(1)
+        
+        if not sys.argv[2].endswith("@kmitl.ac.th"):
+            print("Error: Invalid option or argument. Use --help for usage information.")
+            sys.exit(1)
+
+        if len(sys.argv) < 3:
+            print("Error: Invalid option or argument. Use --help for usage information.")
             sys.exit(1)
         new_data = sys.argv[2]
         add_data(file_path, new_data)
 
-    elif command == "--remove":
-        if len(sys.argv) < 2:
-            print("Usage:--remove [target_data]")
+    elif command == "--remove" or command == "-r":
+        if len(sys.argv[2]) > 76:
+            print("Error: The email address is too long (maximum 76 characters).")
+            sys.exit(1)
+
+        if len(sys.argv) < 3:
+            print("Error: Invalid option or argument. Use --help for usage information.")
             sys.exit(1)
         target_data = sys.argv[2]
         remove_data(file_path, target_data)
 
-    elif command == "--list":
+    elif command == "--list" or command == "-li":
+        if not len(sys.argv) == 2:
+            print("Error: Invalid option or argument. Use --help for usage information.")
+            sys.exit(1)
         list_data(file_path)
 
+    elif command == "--help":
+        print("Usage: analyze_email [OPTIONS]")
+        print()
+        print("analyze_email is a command line instruction using to manage email addresses in analysis.service")
+        print()
+        print("  Option              Long Option                                 Meaning")
+        print("  -li                 --list                                      list all email addresses to service")
+        print("  -a <email address>  --add <email address>                       add email address to service")
+        print("  -r <email address>  --remove <email address>                    remove email address from service")
+        print()
+        print("Specify the email address with @kmitl.ac.th domain")
     else:
-        print("Invalid command. Use --add, --remove, or --list.")
+        print("Error: Invalid option or argument. Use --help for usage information.")
         sys.exit(1)
 
 if __name__ == "__main__":
     main()
-
